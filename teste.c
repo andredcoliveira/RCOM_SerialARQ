4a306f59a4f7fd05c03ad70ee9075c2d74252f6c
RCOM I320 <netedu@gnu34.netlab.fe.up.pt>
2017-11-01 00:32:01 +0000

    teste com datas

--- teste.c
+++ teste.c
@@ -8,15 +8,20 @@
 
 int main(int argc, char** argv) {
 
-  fprintf(stderr, "\n\nsizeof(mode_t): %lu\n\n", sizeof(mode_t));
+  fprintf(stderr, "\n\nsizeof(struct timespec): %lu\n\n", sizeof(struct timespec));
+  fprintf(stderr, "\n\nsizeof(time_t): %lu\n\n", sizeof(time_t));
+  fprintf(stderr, "\n\nsizeof(long): %lu\n\n", sizeof(long));
 
-  int sum = 0, res = 0, i = 0;
-  unsigned char L;
-  unsigned char* V;
+  long sum = 0, res = 0;
+  int i = 0;
+  unsigned char L0, L1;
+  unsigned char *V0, *V1;
+
+  int fdteste;
 
   struct stat fileStat;
-  mode_t file_mode_original;
-  mode_t file_mode_obtido;
+  struct timespec file_time_original[2];
+  struct timespec file_time_obtido[2];
 
   if(argc < 2) {
     perror("nr of arguments");
@@ -28,28 +33,63 @@ int main(int argc, char** argv) {
     return -1;
   }
 
-  file_mode_original = fileStat.st_mode;
+  file_time_original[0] = fileStat.st_atim;
+  file_time_original[1] = fileStat.st_mtim;
 
-  //code
-  L = sizeof(file_mode_original);
-  V = (unsigned char *) malloc(L);
+
+  L0 = sizeof(file_time_original[0].tv_sec) + sizeof(file_time_original[0].tv_nsec);
+  V0 = (unsigned char *) malloc(L0);
   sum = 0;
-  for(i = 0; i < L; i++) {
-    res = (file_mode_original - sum) % (int)pow(256,3-i);
-    V[i] = (file_mode_original - res) / pow(256,3-i) - sum;
-    sum += V[i] * pow(256,3-i);
+  for(i = 0; i < sizeof(file_time_original[0].tv_sec); i++) {
+    res = (file_time_original[0].tv_sec - sum) % (long)pow(256,sizeof(file_time_original[0].tv_sec)-1-i);
+    V0[i] = (file_time_original[0].tv_sec - res) / pow(256,sizeof(file_time_original[0].tv_sec)-1-i) - sum;
+    sum += V0[i] * pow(256,sizeof(file_time_original[0].tv_sec)-1-i);
   }
-  fprintf(stderr, "file_mode_original = %d\n\n", file_mode_original);
+  sum = 0;
+  for(i = sizeof(file_time_original[0].tv_sec); i < L0; i++) {
+    res = (file_time_original[0].tv_nsec - sum) % (long)pow(256,L0-1-i);
+    V0[i] = (file_time_original[0].tv_nsec - res) / pow(256,L0-1-i) - sum;
+    sum += V0[i] * pow(256,L0-1-i);
+  }
 
 
-  //decode
-  file_mode_obtido = pow(256,3) * (int)(V[0]) +
-                     pow(256,2) * (int)(V[1]) +
-                     pow(256,1) * (int)(V[2]) +
-                     pow(256,0) * (int)(V[3]);
-  fprintf(stderr, "file_mode_obtido = %d\n\n", file_mode_obtido);
+  L1 = sizeof(file_time_original[1].tv_sec) + sizeof(file_time_original[1].tv_nsec);
+  V1 = (unsigned char *) malloc(L1);
+  sum = 0;
+  for(i = 0; i < sizeof(file_time_original[1].tv_sec); i++) {
+    res = (file_time_original[1].tv_sec - sum) % (long)pow(256,sizeof(file_time_original[1].tv_sec)-1-i);
+    V1[i] = (file_time_original[1].tv_sec - res) / pow(256,sizeof(file_time_original[1].tv_sec)-1-i) - sum;
+    sum += V1[i] * pow(256,sizeof(file_time_original[1].tv_sec)-1-i);
+  }
+  sum = 0;
+  for(i = sizeof(file_time_original[1].tv_sec); i < L1; i++) {
+    res = (file_time_original[1].tv_nsec - sum) % (long)pow(256,L1-1-i);
+    V1[i] = (file_time_original[1].tv_nsec - res) / pow(256,L1-1-i) - sum;
+    sum += V1[i] * pow(256,L1-1-i);
+  }
 
-  open("test_file", O_CREAT | O_APPEND | O_TRUNC | O_WRONLY, file_mode_obtido);
+
+  file_time_obtido[0].tv_sec = 0;
+  file_time_obtido[0].tv_nsec = 0;
+  file_time_obtido[1].tv_sec = 0;
+  file_time_obtido[1].tv_nsec = 0;
+  for(i = 0; i < 8; i++) {
+    file_time_obtido[0].tv_sec += pow(256,7-i) * (long)V0[i];
+    file_time_obtido[0].tv_nsec += pow(256,7-i) * (long)V0[i+8];
+    file_time_obtido[1].tv_sec += pow(256,7-i) * (long)V1[i];
+    file_time_obtido[1].tv_nsec += pow(256,7-i) * (long)V1[i+8];
+  }
+
+
+  if((fdteste = open("test_file.jpg", O_CREAT | O_APPEND | O_TRUNC | O_WRONLY, fileStat.st_mode)) < 0) {
+    perror("open");
+    exit(-1);
+  }
+
+  if(futimens(fdteste, file_time_obtido) < 0) {
+    perror("futimens");
+    exit(-1);
+  }
 
   return 0;
 
